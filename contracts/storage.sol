82d8d9928fd5b496f8d5f579fb8c433edd52f721
Suryakant-thombre-au8 <suryathombre000@gmail.com>
2023-03-22 17:13:27 +0530

    web3 image share contract complete

--- contracts/storage.sol
+++ contracts/storage.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.7.0 <0.9.0
+pragma solidity >=0.7.0 <0.9.0;
 
 contract Upload {
     struct Access{
@@ -6,9 +6,9 @@ contract Upload {
         bool access;
     }
 
-    mapping(address=>string[) value;
+    mapping(address=>string[]) value;
     mapping(address=>mapping(address=>bool)) ownership;
-    mapping(address=>access[]) accessList;
+    mapping(address=>Access[]) accessList;
     mapping(address=>mapping(address=>bool)) previousData;
 
     function add(address _user, string memory url) external {
@@ -17,11 +17,33 @@ contract Upload {
 
     function allow(address user) external {
         ownership[msg.sender][user]=true;
-
-        accessList[msg.sender].push(Access(user,true));
+        if(previousData[msg.sender][user]){
+            for(uint i=0; i<accessList[msg.sender].length;i++){
+                if(accessList[msg.sender][i].user==user){
+                    accessList[msg.sender][i].access=true;
+                }
+            }
+        }else{
+            accessList[msg.sender].push(Access(user,true));
+            previousData[msg.sender][user]=true;
+        }
     }
 
     function disAllow(address user)public{
         ownership[msg.sender][user]=false;
+        for(uint i=0; i<accessList[msg.sender].length; i++){
+            if(accessList[msg.sender][i].user==user){
+                accessList[msg.sender][i].access=false;
+            }
+        }
+    }
+
+    function display(address _user)external view returns(string[] memory){
+        require(_user==msg.sender || ownership[_user][msg.sender],"You don't have access");
+        return value[_user];
+    }
+
+    function shareAccess() public view returns(Access[] memory){
+        return accessList[msg.sender];
     }
 }
